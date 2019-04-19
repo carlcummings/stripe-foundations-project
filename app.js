cb9a2d4744debaf639e6e0faf5b8b4ae17eba2f0
Carl Cummings <cc@stripe.com>
2019-04-18 19:06:32 -0700

    changed to chocolate

--- app.js
+++ app.js
@@ -4,6 +4,24 @@ function purchaseProduct(event) {
   alert('hello world');
 }
 
-function openProductModal() {
+function openProductModal(id) {
+  console.log(id)
+  let productTitle = document.querySelector(`#${id} .card-title`)
+  let modalTitle = document.querySelector('#productItemTitle')
+  let productDetails = document.querySelector(`#${id} .card-text`)
+  let modalDetails = document.querySelector('#productItemDescription')
+  let productPrice = document.querySelector(`#${id} .card-price`)
+  console.log(productPrice)
+  let modalPrice = document.querySelector('#productItemPrice')
+  console.log(modalPrice.innerHTML=2)
+
+  modalTitle.innerHTML = productTitle.innerHTML
+  modalDetails.innerHTML = productDetails.innerHTML
+  modalPrice.innerHTML = productPrice.innerHTML
+
+
   $('#purchaseProductModal').modal('show')
 }
+
+
+//productItemDescription
